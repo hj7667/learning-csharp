43ac977cdd63cc07c0385617369a25f289a55d10
unknown <chohj.biz@gmail.com>
2026-08-31 08:57:26 +0900

    타입 및 선언

--- Program.cs
+++ Program.cs
@@ -1,4 +1,44 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
 
-Console.ReadKey();
+Console.WriteLine("Hello, World!");
+
+
+string message = "안녕하세요";
+
+Console.WriteLine(message); // 주석
+Console.ReadKey();          // 주석
+
+
+/*
+ 
+ 클래스, 메서드, 인수, 문자열, 선언, 디버깅
+ 
+ 
+ */
+
+
+// 정수형 타입
+
+byte age = 255;
+
+// ㅁㅁㅁㅁㅁㅁㅁㅁ 2^8 = 256
+Console.WriteLine("age: ", age)
+
+
+// 실수
+
+
+// f,d, m
+// 부동 소수, 고정 소수 의 차이 
+// 더블 
+
+// bool , char 타입
+
+// 유니코드 표현식.,
+
+// enum 왜쓰는가
+
+// 명시적 선언 (Explicit Declaration)
+// 암시적 선언 (Implicit Declaration)
+// 의 장다넞ㅁ
 
