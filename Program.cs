f784a0dc873b80772be134c54bb4e5fb873d5bc9
unknown <chohj.biz@gmail.com>
2026-09-02 17:03:50 +0900

    인터페이스, 제너릭, 구현체

--- Program.cs
+++ Program.cs
@@ -1,26 +1,4 @@
-﻿
-int a = 1;
-int b = 2;
-
-if (a == b) {
-    Console.WriteLine("a와 같다");
-
-} else {
-    Console.WriteLine("a 와 b는 다르다");
-}
+﻿// Program.cs (진입점은 여기 하나만)
+Practice.Run();
 
 Console.ReadKey();
-
-//switch 문 표현식
-
-string grade = "F";
-
-string message = grade switch
-{
-    "A" => "우수한 성적입니다",
-    string g when g == "B" || g == "B+" => "좋은 성적입니다",
-    "c" => "보통 성적입니다.",
-    _ => "잘 모르겠습니다"
-};
-
-Console.WriteLine(message);
